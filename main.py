608a10e17dcf0baa2becb355dbe297364051124c
Gekcer <gekcer@gmail.com>
2024-03-07 22:03:36 +0300

    чистка класса url_parser

--- main.py
+++ main.py
@@ -9,15 +9,16 @@ if __name__ == "__main__":
     html_manager = HtmlManager()
     html_manager.work_with_html(file_name)
     urls = html_manager.get_all_url_matches()
-
+    print('Достали url')
     # Проверка подключения
     # html_manager._all_url_connection_check()
 
     # Записиали html
     folder = 'matches_html'
-    url_parser = UrlParser('chrome')
-    url_parser.parse_htmls(urls[:3], folder)
+    url_parser = UrlParser('firefox')
+    url_parser.parse_htmls(urls[:20], folder)
 
+    soups = url_parser.get_all_soups()
     #url_parser.get_all_htmls_from_urls(urls[:3], folder)
     #url_parser.get_soup_from_html('matches_html', )
     #url_parser.set_all_soups_from_html(folder)
