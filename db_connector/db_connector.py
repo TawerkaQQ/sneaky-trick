7f5c57505f09046fab1c6f7f689cf5e4840f219b
Gekcer <gekcer@gmail.com>
2024-03-11 23:02:43 +0300

    фиксы + чистка

--- db_connector/db_connector.py
+++ db_connector/db_connector.py
@@ -8,17 +8,17 @@ class DBConnector:
     def __init__(self):
         load_dotenv()
         self.DB_NAME = os.environ.get("DB_NAME")
-        self.dsn = f'sqlite:///{self.DB_NAME}'
+        self.dsn = self.get_dsn()
         self.engine = create_engine(self.get_dsn(), echo=False)
         self.Session = sessionmaker(bind=self.get_engine())
 
     def get_dsn(self):
-        return self.dsn
+        dsn = f'sqlite:///{self.DB_NAME}'
+        return dsn
 
     def get_engine(self):
         return self.engine
 
     def get_session(self):
-        return self.Session
-
-
+        Session = sessionmaker(bind=self.engine)
+        return Session()
--- db_connector/models.py
+++ db_connector/models.py
@@ -3,9 +3,18 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class Match(Base):
     __tablename__ = "matches"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     l_team_name = Column(String)
     r_team_name = Column(String)
+
+
+class Test_table(Base):
+    __tablename__ = 'test_table'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    age = Column(Integer)
