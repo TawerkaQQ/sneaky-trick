9801e6ce0d66e224b23e82820e8f048f5caf036c
Ivan <ivkalinin@sfedu.ru>
2024-03-23 23:27:06 +0300

    modify main

--- main.py
+++ main.py
@@ -6,19 +6,23 @@ from match import Match
 import json
 
 
-with open('config.json', 'r') as f:
-    data = json.load(f)
-    print(data["file_name"])
+
         
 class MainController:
     def __init__(self):
-        self.file_name = data['file_name']
-        self.folder = data['folder']
+        self.data = self.setup_config_json()
+        self.file_name = self.data['file_name']
+        self.folder = self.data['folder']
         self.html_manager = HtmlManager()
-        self.url_parser = UrlParser(data['browser'])
-        self.storage = Storage(data['sprot_type'])
+        self.url_parser = UrlParser(self.data['browser'])
+        self.storage = Storage(self.data['sprot_type'])
         self.urls = []
         self.soups = []
+
+    def setup_config_json(self):
+        with open('config.json', 'r') as f:
+            data = json.load(f)
+        return data
 
     # # Достали url
     def get_url_from_file(self):
@@ -33,11 +37,11 @@ class MainController:
 
     # Записиали html
     def set_html_from_ulr(self):
-        self.url_parser.parse_htmls(self.urls[:int(data['nums_url'])], self.folder)
+        self.url_parser.parse_htmls(self.urls[:int(self.data['nums_url'])], self.folder)
         return self
 
     def set_soups(self):
-        self.soups = self.url_parser.get_all_soups()[:int(data['nums_url'])]
+        self.soups = self.url_parser.get_all_soups()[:int(self.data['nums_url'])]
         return self
     # url_parser.get_all_htmls_from_urls(urls[:3], folder)
     # url_parser.get_soup_from_html('matches_html', )
